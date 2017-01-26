27a43d2cba2e1c932598718065422e84ff5ccc6a
Willian Galvani <williangalvani@gmail.com>
2017-01-26 16:15:57 -0200

    change allowed hosts
    Signed-off-by: Willian Galvani <williangalvani@gmail.com>

--- tomadaBackend/settings.py
+++ tomadaBackend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'v@n+ifm+zba^8-4&nc)=olb+oi%$p)dp$v$zz!8mvwk*0aa4)='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["ibexcps.com", "localhost"]
 
 
 # Application definition
